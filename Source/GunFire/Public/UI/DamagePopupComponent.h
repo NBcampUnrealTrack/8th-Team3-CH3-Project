39a4eee54274c7d45ced267c8f218ea3200c8bb3
LeeDongHyun <aopo3948@gmail.com>
2026-05-19 14:47:50 +0900

    데미지 팝업 표시 구현
    - StatComponent의 OnDamaged 델리게이트를 이용해서 데미지 표시 구현
    - DamagePopupComponent에서 피격 이벤트 바인딩 및 팝업 액터 생성
    - DamagePopupActor와 WBP를 통해 데미지 텍스트와 애니메이션 처리
    - 애니메이션 종료 시 DamagePopupActor를 자동 제거하도록 함

---
+++ Source/GunFire/Public/UI/DamagePopupComponent.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DamagePopupComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable )
+class GUNFIRE_API UDamagePopupComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UDamagePopupComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void HandleDamaged(float ReceivedDamage, AController* DamageInstigator);
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "UI")
+    void ShowDamagePopup(float ReceivedDamage, AController* DamageInstigator, FVector SpawnLocation);
+
+protected:
+    // 생성할 위치
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    FVector SpawnOffset;
+
+    // 랜덤 반경
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    float RandomOffsetRadius;
+};
